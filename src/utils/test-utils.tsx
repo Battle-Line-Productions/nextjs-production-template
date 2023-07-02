6392e5d9468855ebd1bf9bb374d054df3387fd70
Michael Cavanaugh <funkel1989@gmail.com>
2023-07-02 16:52:29 -0400

    adding storybook

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { Provider as StoreProvider } from "react-redux";
-import { render, RenderOptions } from "@testing-library/react";
+import { render, RenderOptions, RenderResult } from "@testing-library/react";
 import { store } from "@/store";
 
 const Providers = ({ children }: { children: React.ReactElement }) => {
 	return <StoreProvider store={store}>{children}</StoreProvider>;
 };
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
+const customRender = (ui: React.ReactElement, options?: RenderOptions): RenderResult =>
 	render(ui, { wrapper: Providers, ...options });
 
 export * from "@testing-library/react";
+
 export { customRender as render };
